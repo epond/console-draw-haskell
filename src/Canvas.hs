ea9c3beb0457ab167d38e04f043f8d6bb0f4aace
epond <epond@users.noreply.github.com>
2015-01-26 22:00:30 +0000

    Note

--- src/Canvas.hs
+++ src/Canvas.hs
@@ -1,5 +1,7 @@
 module Canvas where
 
+-- In Scala this contained rows: Vector[Vector[Char]]
+-- see https://www.haskell.org/tutorial/arrays.html
 data Canvas = Canvas deriving Show
 
 emptyCanvas :: Canvas
