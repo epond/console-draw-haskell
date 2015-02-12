96cddbb6bf67f0bd73057383cbcd85c0d9d503e7
epond <epond@users.noreply.github.com>
2015-02-12 23:06:46 +0000

    Implement command parser

--- src/Command.hs
+++ src/Command.hs
@@ -1,7 +1,13 @@
 module Command where
 
 parseCommand :: String -> Maybe CanvasCommand
-parseCommand _ = Just ClearCommand
+parseCommand commandString = case (words commandString) of
+    "C" : w : h : [] -> Just $ NewCanvasCommand (read w) (read h)
+    "L" : x1 : y1 : x2 : y2 : [] -> Just $ DrawLineCommand (Coordinates (read x1) (read y1)) (Coordinates (read x2) (read y2))
+    "R" : x1 : y1 : x2 : y2 : [] -> Just $ DrawRectangleCommand (Coordinates (read x1) (read y1)) (Coordinates (read x2) (read y2))
+    "B" : x : y : c : [] -> Just $ BucketFillCommand (Coordinates (read x) (read y)) (head c)
+    "CLR" : [] -> Just ClearCommand
+    _ -> Nothing
 
 data CanvasCommand =
     NewCanvasCommand{width :: Int, height :: Int} |
--- test/CommandSpec.hs
+++ test/CommandSpec.hs
@@ -8,5 +8,13 @@ import Command
 spec :: Spec
 spec = do
     describe "parseCommand" $ do
-        it "can parse a new canvas" $ do
+        it "can parse a new canvas command" $ do
             parseCommand "C 20 4" `shouldBe` Just (NewCanvasCommand 20 4)
+        it "can parse a draw line command" $ do
+            parseCommand "L 2 1 2 4" `shouldBe` Just (DrawLineCommand (Coordinates 2 1) (Coordinates 2 4))
+        it "can parse a draw rectangle command" $ do
+            parseCommand "R 1 1 3 4" `shouldBe` Just (DrawRectangleCommand (Coordinates 1 1) (Coordinates 3 4))
+        it "can parse a bucket fill command" $ do
+            parseCommand "B 3 4 o" `shouldBe` Just (BucketFillCommand (Coordinates 3 4) 'o')
+        it "can parse a clear command" $ do
+            parseCommand "CLR" `shouldBe` Just ClearCommand
