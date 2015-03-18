b876904688bcb7f893dfa0b4824031806bfdc8bd
epond <epond@users.noreply.github.com>
2015-03-18 21:12:54 +0000

    Beginnings of BucketFill

--- src/BucketFill.hs
+++ src/BucketFill.hs
@@ -2,6 +2,13 @@ module BucketFill where
 
 import Common
 import Canvas
+import qualified Data.Set as Set
 
 bucketFill :: Coordinates -> Char -> Canvas -> ColourLayer
-bucketFill _ _ _ = emptyLayer
+bucketFill origin colour canvas =
+    let layerPoints = fillAcc canvas Set.empty [origin]
+    in ColourLayer (Set.toList layerPoints) colour
+
+-- https://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Set.html
+fillAcc :: Canvas -> Set.Set Coordinates -> [Coordinates] -> Set.Set Coordinates
+fillAcc _ _ _ = Set.empty
